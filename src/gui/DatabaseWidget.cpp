564559e0d8582aed3cbefa8a8d1ecdfbc74677ba
Jonathan White <support@dmapps.us>
2025-06-19 07:50:35 -0400

    code format

--- src/gui/DatabaseWidget.cpp
+++ src/gui/DatabaseWidget.cpp
@@ -2689,14 +2689,14 @@ bool DatabaseWidget::hasRecycledSelectedEntries() const
     if (!m_entryView) {
         return false;
     }
-    
+
     // Check if any of the selected entries are actually recycled
     for (auto* entry : m_entryView->selectedEntries()) {
         if (entry && entry->isRecycled()) {
             return true;
         }
     }
-    
+
     return false;
 }
 
